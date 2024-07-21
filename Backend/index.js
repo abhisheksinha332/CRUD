f5466e973e9e92163d5332887531167aab421068
abhisheksinha332 <3327abhishek@gmail.com>
2024-07-21 21:25:12 +0530

    Added MongoDB connection

--- Backend/index.js
+++ Backend/index.js
@@ -1,4 +1,6 @@
 import express from 'express'
+import mongoose from 'mongoose';
+
 
 console.log("hello world");
 
@@ -7,9 +9,17 @@ const app = express();
 const PORT = 3000;
 
 app.get('/', function(req,res){
-    res.send("Hello world")
+    res.send("Hello world, I am back to My web development !!")
 })
-app.listen(PORT, function(err){
+
+mongoose.connect("mongodb+srv://3327abhishek:hW9JLXPMuiLMOJ5T@test1.wljekw7.mongodb.net/FirstDatabase/FirstCollection?retryWrites=true&w=majority&appName=Test1",{}).then( app.listen(PORT, function(err){
     if (err) console.log("Error in server setup")
         console.log("Server listening on Port", PORT);
-})
+}))
+
+
+try {
+    
+} catch (error) {
+    
+}
